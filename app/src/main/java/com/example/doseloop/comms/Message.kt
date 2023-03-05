6d04f60ad433e115d950a1449b38cfdd3facfbdc
Niklas Seppala <niklas.t.seppala@gmail.com>
2023-03-05 19:36:11 +0200

    remove stupid stuff

--- app/src/main/java/com/example/doseloop/comms/Message.kt
+++ app/src/main/java/com/example/doseloop/comms/Message.kt
@@ -1,52 +1,42 @@
 package com.example.doseloop.comms
 
-interface MessageType<T : MessageType<T>> {
-    var payload: String
-    val code: Int
+interface MessageType {
+    fun sms(): String;
+    fun json(): String;
+}
 
-    fun sms(): String = "!${code}=${payload}!"
-    fun json(): String = "{ \"code\": $code, \"payload\": \"${payload}\" }" // :)
+enum class Message(private val code: Int, private var payload: String = "") :
+    MessageType {
+    ALARM_MISSED_MEDS(120),
+    ALARM_BUTTON_PRESS(121),
+    ALARM_FIRE_WATER(122),
+    ALARM_MISSED_FOOD(123),
 
-    @Suppress("UNCHECKED_CAST")
-    fun withPayload(payload: String): T {
+    PHONE_SET_1(110),
+    PHONE_SET_2(111),
+    PHONE_SET_3(112),
+    PHONE_SET_4(113),
+    PHONE_SET_5(114);
+
+    override fun sms(): String = "!${code}=${payload}!"
+    override fun json(): String = "{ \"code\": $code, \"payload\": \"${payload}\" }" // :)
+    fun withPayload(payload: String): Message {
         this.payload = payload
-        return this as T;
+        return this;
+    }
+    companion object {
+        const val DISABLE = ""
     }
 }
-
-object Payload {
-    const val DISABLE_FEATURE = ""
-}
-
-enum class AlarmMessage(override val code: Int, override var payload: String = "") :
-    MessageType<AlarmMessage> {
-    MISSED_MEDS(120),
-    BUTTON_PRESS(121),
-    FIRE_WATER_ALARM(122),
-    MISSED_FOOD(123);
-}
-
-
-enum class PhoneNumberMessage(override val code: Int, override var payload: String = "") :
-    MessageType<PhoneNumberMessage> {
-    SET_PHONE_1(110),
-    SET_PHONE_2(111),
-    SET_PHONE_3(112),
-    SET_PHONE_4(113),
-    SET_PHONE_5(114);
-}
-
 
 class PhoneNumber(private val number: String) : Destination {
     private fun getFinnishPhone(): String {
         // TODO: DO SOME REAL STUFF
         return "+358${number.substring(1, number.length)}"
     }
-
     fun normalize(): String = getFinnishPhone()
     override fun getAddress(): String = normalize()
 }
-
 
 
 interface Destination {
@@ -63,32 +53,30 @@ class HttpDestination(private val url: String): Destination {
     }
 }
 
-
-
 interface MessageService {
-    fun<T: MessageType<T>> sendMessage(msg: MessageType<T>)
+    fun sendMessage(msg: MessageType)
 }
 
 class GsmMessageService(val destination: Destination): MessageService {
-    override fun <T : MessageType<T>> sendMessage(msg: MessageType<T>) {
+    override fun sendMessage(msg: MessageType) {
         println("[${this::class.simpleName}] : [${msg::class.simpleName}] -- ${msg.sms()}")
     }
 }
 
 class RestMessageService(val destination: Destination): MessageService {
-    override fun <T : MessageType<T>> sendMessage(msg: MessageType<T>) {
+    override fun sendMessage(msg: MessageType) {
         println("[${this::class.simpleName}] : [${msg::class.simpleName}] -- ${msg.json()}")
     }
 }
 
 
-
 fun main() {
-    val alarm1 = AlarmMessage.BUTTON_PRESS.withPayload("You pressed the button")
-    val alarm2 = AlarmMessage.MISSED_FOOD.withPayload("Eat yer food, grandma")
-    val alarm3 = AlarmMessage.MISSED_MEDS.withPayload(Payload.DISABLE_FEATURE)
-    val phone1 = PhoneNumberMessage.SET_PHONE_1.withPayload(PhoneNumber("0204521350").normalize())
-    val phone2 = PhoneNumberMessage.SET_PHONE_2.withPayload(PhoneNumber("0504525780").normalize())
+    val alarm1 = Message.ALARM_BUTTON_PRESS.withPayload("You pressed the button")
+    val alarm2 = Message.ALARM_MISSED_FOOD.withPayload("Eat yer food, grandma")
+    val alarm3 = Message.ALARM_MISSED_MEDS.withPayload(Message.DISABLE)
+
+    val phone1 = Message.PHONE_SET_1.withPayload(PhoneNumber("0204521350").normalize())
+    val phone2 = Message.PHONE_SET_2.withPayload(PhoneNumber("0504525780").normalize())
 
     val msgService = GsmMessageService(GsmDestination(PhoneNumber("05048569823")))
     msgService.sendMessage(alarm1)
